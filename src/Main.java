52393dfed847673ec5b7916c6bac4cd0f8a6d22e
HernanGoldy <hernanmdq@gmail.com>
2024-06-02 20:33:59 -0300

    inicio del proyecto

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido a Screen Match");
+        System.out.println("Película: Matrix");
+
+        // attributes
+        int fechaDeLanzamiento = 1999;
+        boolean iuncluidoEnElPlan = true;
+        double notaDeLaPelicula = 8.2;
+
+        double media = (8.2 + 6.0 + 9.0) / 3;
+        System.out.println(media);
+    }
+}
